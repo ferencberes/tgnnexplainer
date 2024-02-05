ee99d0bb57e51000772bb6142b88ab5bbf83894c
Jesse Wonnink <scur1016@int5.local.snellius.surf.nl>
2024-02-05 22:27:40 +0100

    fixed tgn run jobs

--- code/TGNNEXPLAINER-PUBLIC/tgnnexplainer/xgraph/models/ext/tgn/trainnew.sh
+++ code/TGNNEXPLAINER-PUBLIC/tgnnexplainer/xgraph/models/ext/tgn/trainnew.sh
@@ -6,11 +6,11 @@ do
     echo "${i}-th run\n"
 
     dataset=mooc
-    python train_simulate.py -d ${dataset} --prefix tgn-attn --n_runs 1 --n_epoch 10 --n_layer 2 --n_degree 10 --use_memory --node_dim 4 --time_dim 4 --message_dim 4 --memory_update_at_end --gpu 0 \
+    python train_self_supervised.py -d ${dataset} --prefix tgn-attn --n_runs 1 --n_epoch 10 --n_layer 2 --n_degree 10 --use_memory --node_dim 4 --time_dim 4 --message_dim 4 --memory_update_at_end --gpu 0 \
     --memory_dim 88 # memory_dim should equal to node/edge feature di
     
     dataset=reddit_hyperlinks
-    python train_simulate.py -d ${dataset} --prefix tgn-attn --n_runs 1 --n_epoch 10 --n_layer 2 --n_degree 10 --use_memory --node_dim 88 --time_dim 88 --message_dim 88 --memory_update_at_end --gpu 0 \
+    python train_self_supervised.py -d ${dataset} --prefix tgn-attn --n_runs 1 --n_epoch 10 --n_layer 2 --n_degree 10 --use_memory --node_dim 88 --time_dim 88 --message_dim 88 --memory_update_at_end --gpu 0 \
     --memory_dim 4 # memory_dim should equal to node/edge feature di
 
 done
