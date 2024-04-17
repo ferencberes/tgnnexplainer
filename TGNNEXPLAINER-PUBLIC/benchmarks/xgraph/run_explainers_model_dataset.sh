5e1e12401ab5062aa513f833ba5f4f0f3ef6c39b
Christina Isaicu <c.isaicu@gmail.com>
2024-04-17 19:35:31 +0200

    fix seed path on explainers chkpt

--- TGNNEXPLAINER-PUBLIC/benchmarks/xgraph/run_explainers_model_dataset.sh
+++ TGNNEXPLAINER-PUBLIC/benchmarks/xgraph/run_explainers_model_dataset.sh
@@ -1,7 +1,7 @@
 # source /sw/arch/RHEL8/EB_production/2023/software/Anaconda3/2023.07-2/etc/profile.d/conda.sh
 # conda activate condafact
 
-# call this script with `bash run_explainers_model_dataset.sh 0 tgat wikipedia` to on the first GPU with dataset wikipedia
+# call this script with `bash run_explainers_model_dataset.sh 0 tgat wikipedia 2020` to on the first GPU with dataset wikipedia with seed 2020
 
 
 # run all explainers
@@ -11,27 +11,28 @@
 gpu=$1
 model=$2
 dataset=$3
+seed=$4
 
 # HOW TO ADD GPU?????!
 
 #Necessary to train first
-echo "=== STARTING pg_explainer explaining on ${model} trained on ${dataset} on GPU ${gpu} ==="
-python subgraphx_tg_run.py datasets=${dataset} device_id=${gpu} explainers=pg_explainer_tg models=${model}
-echo "=== STARTING pg_explainer explaining on ${model} trained on ${dataset} on GPU ${gpu} ==="
+echo "=== STARTING pg_explainer explaining on ${model} trained on ${dataset} on GPU ${gpu} with seed ${seed}==="
+python subgraphx_tg_run.py datasets=${dataset} device_id=${gpu} explainers=pg_explainer_tg models=${model} seed=${seed}
+echo "=== STARTING pg_explainer explaining on ${model} trained on ${dataset} on GPU ${gpu} with seed ${seed}==="
 
 # ours
-echo "=== STARTING subgraphx explaining on ${model} trained on ${dataset} on GPU ${gpu} ==="
-python subgraphx_tg_run.py  datasets=${dataset} device_id=${gpu} explainers=subgraphx_tg models=${model}
-echo "=== ENDING subgraphx_tg explaining on ${model} trained on ${dataset} on GPU ${gpu} ==="
+echo "=== STARTING subgraphx explaining on ${model} trained on ${dataset} on GPU ${gpu} with seed ${seed}==="
+python subgraphx_tg_run.py  datasets=${dataset} device_id=${gpu} explainers=subgraphx_tg models=${model} seed=${seed}
+echo "=== ENDING subgraphx_tg explaining on ${model} trained on ${dataset} on GPU ${gpu} with seed ${seed}==="
 
 # baselines
-echo "=== STARTING attn explaining on ${model} trained on ${dataset} on GPU ${gpu} ==="
-python subgraphx_tg_run.py datasets=${dataset} device_id=${gpu} explainers=attn_explainer_tg models=${model}
-echo "=== ENDING attn_explainer_tg explaining on ${model} trained on ${dataset} on GPU ${gpu} ==="
+echo "=== STARTING attn explaining on ${model} trained on ${dataset} on GPU ${gpu} with seed ${seed}==="
+python subgraphx_tg_run.py datasets=${dataset} device_id=${gpu} explainers=attn_explainer_tg models=${model} seed=${seed}
+echo "=== ENDING attn_explainer_tg explaining on ${model} trained on ${dataset} on GPU ${gpu} with seed ${seed}==="
 
-echo "=== STARTING pbone explaining on ${model} trained on ${dataset} on GPU ${gpu} ==="
-python subgraphx_tg_run.py datasets=${dataset} device_id=${gpu} explainers=pbone_explainer_tg models=${model}
-echo "=== ENDING pbone explaining on ${model} trained on ${dataset} on GPU ${gpu} ==="
+echo "=== STARTING pbone explaining on ${model} trained on ${dataset} on GPU ${gpu} with seed ${seed}==="
+python subgraphx_tg_run.py datasets=${dataset} device_id=${gpu} explainers=pbone_explainer_tg models=${model} seed=${seed}
+echo "=== ENDING pbone explaining on ${model} trained on ${dataset} on GPU ${gpu} with seed ${seed} ==="
 
 echo "good job :)"
 
--- TGNNEXPLAINER-PUBLIC/benchmarks/xgraph/subgraphx_tg_run.py
+++ TGNNEXPLAINER-PUBLIC/benchmarks/xgraph/subgraphx_tg_run.py
@@ -68,7 +68,7 @@ def pipeline(config: DictConfig):
     config.models.param = config.models.param[config.datasets.dataset_name]
     # config.models.ckpt_path = str(ROOT_DIR/'xgraph'/'models'/'checkpoints'/f'{config.models.model_name}_{config.datasets.dataset_name}_best.pth')
     # TO DO --> move checkpoints all into one dir?????
-    config.models.ckpt_path = str(ROOT_DIR/'xgraph'/'models'/'ext'/f'{config.models.model_name}'/'saved_models'/f'{config.models.model_name}_{config.datasets.dataset_name}_{config.datasets.seed}_best.pth')
+    config.models.ckpt_path = str(ROOT_DIR/'xgraph'/'models'/'ext'/f'{config.models.model_name}'/'saved_models'/f'{config.models.model_name}_{config.datasets.dataset_name}_{config.seed}_best.pth')
 
     # dataset config
     config.datasets.dataset_path = str(ROOT_DIR/'xgraph'/'dataset'/'data'/f'{config.datasets.dataset_name}.csv')
